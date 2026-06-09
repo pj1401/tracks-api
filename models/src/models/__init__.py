2d49a4f2c5fd14405995eae7cdacb51b827e55b4
Patricia Johansson <pj222uc@student.lnu.se>
2026-06-09 19:03:04 +0200

    Fix models import error (#13)

--- models/src/models/__init__.py
+++ models/src/models/__init__.py
@@ -1,7 +1,7 @@
 # type: ignore
 # ruff: disable[F401]
 from .album import Album
-from .artist import Artist
+from .artist import Artist, artists_tracks_table
 from .base import BaseModel
-from .track import Track
+from .track import Track, tracks_albums_table
 from .user import User
