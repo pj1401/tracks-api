c64a94a7f55c1575860b5ade485dc9aceaa023cf
Patricia Johansson <pj222uc@student.lnu.se>
2026-06-11 12:52:16 +0200

    Remove async __init__ in AsyncDatabaseConnectionManager (#21)

--- tracks-api/src/db/async_connection_manager.py
+++ tracks-api/src/db/async_connection_manager.py
@@ -3,11 +3,9 @@ AsyncDatabaseConnectionManager class.
 module: src/db/async_connection_manager.py
 """
 
-from typing import AsyncGenerator, TypeVar
+from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from models import BaseModel
-
-TModel = TypeVar("TModel", bound=BaseModel)
 
 
 class AsyncDatabaseConnectionManager:
@@ -15,7 +13,7 @@ class AsyncDatabaseConnectionManager:
     Manage SQLAlchemy async sessions.
     """
 
-    async def __init__(self, db_uri: str, base_model: type[TModel]):
+    def __init__(self, db_uri: str):
         """
         Initialise the engine.
         """
@@ -23,12 +21,25 @@ class AsyncDatabaseConnectionManager:
         self.async_session_factory = async_sessionmaker(
             bind=self.engine, expire_on_commit=False
         )
-        await self._create_tables(base_model)
 
-    async def _create_tables(self, base_model: type[TModel]) -> None:
+    @classmethod
+    async def create(
+        cls, db_uri: str, base_model: type[BaseModel]
+    ) -> "AsyncDatabaseConnectionManager":
+        instance = cls(db_uri)
+        await instance._create_tables(base_model)
+        return instance
+
+    async def _create_tables(self, base_model: type[BaseModel]) -> None:
         async with self.engine.begin() as conn:
             await conn.run_sync(base_model.metadata.create_all)
 
     async def get_session(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.async_session_factory() as session:
             yield session
+
+    async def dispose(self) -> None:
+        """
+        Dispose of the connection pool used by this engine.
+        """
+        await self.engine.dispose()
