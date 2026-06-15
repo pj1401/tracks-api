86206edefaf0cc2f6708e49f7d837ccb2c2a4480
Patricia Johansson <pj222uc@student.lnu.se>
2026-06-15 16:10:49 +0200

    Update version tag (#51)

--- tracks-api/src/apis/v1_api.py
+++ tracks-api/src/apis/v1_api.py
@@ -1,11 +1,11 @@
 """
 Version 1 sub API application.
-module: src.apis.api_v1
+module: src.apis.v1_api
 """
 
 from fastapi import FastAPI
 from ..routers.api.v1.v1_router import v1_router
 
-api_v1 = FastAPI()
+api_v1 = FastAPI(title="Tracks API v1", version="0.2.0")
 
 api_v1.include_router(v1_router)
--- tracks-api/src/routers/api/v1/v1_router.py
+++ tracks-api/src/routers/api/v1/v1_router.py
@@ -1,20 +1,23 @@
 """
-API version 1 router. Registers API paths.
+API version 1 router. Registers API paths for version 1 of the API.
 module: src/routers/api/v1/v1_router.py
 """
 
-from fastapi import APIRouter
-from .track_router import track_router
-
+from typing import Annotated
+from fastapi import APIRouter, Depends, Request
+from ..collections.track_router import track_router
+from src.config import Settings
+from src.dependencies import get_settings
 
 v1_router = APIRouter()
 v1_router.include_router(track_router, prefix="/tracks")
 
 
 @v1_router.get("/")
-async def get():
+async def get(settings: Annotated[Settings, Depends(get_settings)], request: Request):
     return {
         "message": "Welcome to version 1 of the Tracks API!",
+        "docs": f"{settings.base_url}{request.scope.get('root_path')}/docs",
     }
 
 
