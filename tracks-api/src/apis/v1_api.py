45ebcedde62047b8ab1a0fbabd93a93a933c02cc
Patricia Johansson <pj222uc@student.lnu.se>
2026-06-16 17:34:31 +0200

    Include exception handlers on apps (#61)

--- tracks-api/src/apis/v1_api.py
+++ tracks-api/src/apis/v1_api.py
@@ -4,8 +4,11 @@ module: src.apis.v1_api
 """
 
 from fastapi import FastAPI
+from src.decorators.exception_handlers import setup_exception_handlers
 from ..routers.api.v1.v1_router import v1_router
 
 api_v1 = FastAPI(title="Tracks API v1", version="0.2.0")
 
 api_v1.include_router(v1_router)
+
+setup_exception_handlers(api_v1)
--- tracks-api/src/decorators/exception_handlers.py
+++ tracks-api/src/decorators/exception_handlers.py
@@ -3,17 +3,16 @@ Handle exceptions.
 module: src.decorators.exception_handlers
 """
 
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from src.util.error import httpStatusReasonMap, log_original_error
 
 
 def setup_exception_handlers(app: FastAPI):
     @app.exception_handler(RequestValidationError)
     async def validation_exception_handler(  # type: ignore[unused-ignore]
-        request: Request,
-        exc: RequestValidationError,
-        response: Response,
+        request: Request, exc: RequestValidationError
     ):
         for error in exc.errors():
             log_original_error(error)
@@ -25,5 +24,4 @@ def setup_exception_handlers(app: FastAPI):
                 "The server encountered an unexpected condition that prevented it from fulfilling the request.",
             ),
         }
-        response.status_code = http_status
-        return response_data
+        return JSONResponse(response_data, status_code=http_status)
